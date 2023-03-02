512ee6fca0570af6014085871c394d8d03c8a1ec
tech-interface <sanketpatilcs@gmail.com>
2023-03-02 20:36:05 +0530

    Sample App to deploy

---
+++ spring-boot-railway-app/src/main/java/com/techinterface/SpringBootRailwayAppApplication.java
@@ -0,0 +1,13 @@
+package com.techinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRailwayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRailwayAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-railway-app/src/test/java/com/techinterface/SpringBootRailwayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRailwayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
